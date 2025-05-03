040eae311149fe5975d013b1cc6fb098d3868af0
liebman <liebman@zod.com>
2025-05-03 09:58:05 -0700

    added first cut at documentation

--- src/lib.rs
+++ src/lib.rs
@@ -95,6 +95,7 @@
 //! - Proper error propagation
 
 #![no_std]
+#![warn(missing_docs)]
 // MUST be the first module
 mod fmt;
 
@@ -111,48 +112,85 @@ use embedded_hal_async::i2c::I2c;
 
 use crate::datetime::DS3231DateTime;
 
+/// Configuration for the DS3231 RTC device.
+/// 
+/// This struct contains all configurable parameters for the DS3231 device,
+/// including time representation format, square wave output settings,
+/// and oscillator control.
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Config {
+    /// Time representation format (12-hour or 24-hour)
     pub time_representation: TimeRepresentation,
+    /// Frequency of the square wave output
     pub square_wave_frequency: SquareWaveFrequency,
+    /// Control mode for the interrupt output pin
     pub interrupt_control: InterruptControl,
+    /// Enable square wave output when running on battery power
     pub battery_backed_square_wave: bool,
+    /// Enable or disable the oscillator
     pub oscillator_enable: Ocillator,
 }
 
+/// Register addresses for the DS3231 RTC.
 #[allow(unused)]
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum RegAddr {
+    /// Seconds register (0-59)
     Seconds = 0x00,
+    /// Minutes register (0-59)
     Minutes = 0x01,
+    /// Hours register (1-12 + AM/PM or 0-23)
     Hours = 0x02,
+    /// Day register (1-7)
     Day = 0x03,
+    /// Date register (1-31)
     Date = 0x04,
+    /// Month register (1-12)
     Month = 0x05,
+    /// Year register (0-99)
     Year = 0x06,
+    /// Alarm 1 seconds register
     Alarm1Seconds = 0x07,
+    /// Alarm 1 minutes register
     Alarm1Minutes = 0x08,
+    /// Alarm 1 hours register
     Alarm1Hours = 0x09,
+    /// Alarm 1 day/date register
     Alarm1DayDate = 0x0A,
+    /// Alarm 2 minutes register
     Alarm2Minutes = 0x0B,
+    /// Alarm 2 hours register
     Alarm2Hours = 0x0C,
+    /// Alarm 2 day/date register
     Alarm2DayDate = 0x0D,
+    /// Control register
     Control = 0x0E,
+    /// Control/Status register
     ControlStatus = 0x0F,
+    /// Aging offset register
     AgingOffset = 0x10,
+    /// Temperature MSB register
     MSBTemp = 0x11,
+    /// Temperature LSB register
     LSBTemp = 0x12,
 }
 
+/// Time representation format for the DS3231.
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum TimeRepresentation {
+    /// 24-hour format (0-23)
     TwentyFourHour = 0,
+    /// 12-hour format (1-12 + AM/PM)
     TwelveHour = 1,
 }
 impl From<u8> for TimeRepresentation {
+    /// Creates a TimeRepresentation from a raw register value.
+    ///
+    /// # Panics
+    /// Panics if the value is not 0 or 1.
     fn from(v: u8) -> Self {
         match v {
             0 => TimeRepresentation::TwentyFourHour,
@@ -162,18 +200,26 @@ impl From<u8> for TimeRepresentation {
     }
 }
 impl From<TimeRepresentation> for u8 {
+    /// Converts a TimeRepresentation to its raw register value.
     fn from(v: TimeRepresentation) -> Self {
         v as u8
     }
 }
 
+/// Oscillator control for the DS3231.
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum Ocillator {
+    /// Oscillator is enabled
     Enabled = 0,
+    /// Oscillator is disabled
     Disabled = 1,
 }
 impl From<u8> for Ocillator {
+    /// Creates an Ocillator from a raw register value.
+    ///
+    /// # Panics
+    /// Panics if the value is not 0 or 1.
     fn from(v: u8) -> Self {
         match v {
             0 => Ocillator::Enabled,
@@ -183,18 +229,26 @@ impl From<u8> for Ocillator {
     }
 }
 impl From<Ocillator> for u8 {
+    /// Converts an Ocillator to its raw register value.
     fn from(v: Ocillator) -> Self {
         v as u8
     }
 }
 
+/// Interrupt control mode for the DS3231.
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum InterruptControl {
+    /// Output square wave on INT/SQW pin
     SquareWave = 0,
+    /// Output interrupt signal on INT/SQW pin
     Interrupt = 1,
 }
 impl From<u8> for InterruptControl {
+    /// Creates an InterruptControl from a raw register value.
+    ///
+    /// # Panics
+    /// Panics if the value is not 0 or 1.
     fn from(v: u8) -> Self {
         match v {
             0 => InterruptControl::SquareWave,
@@ -204,20 +258,30 @@ impl From<u8> for InterruptControl {
     }
 }
 impl From<InterruptControl> for u8 {
+    /// Converts an InterruptControl to its raw register value.
     fn from(v: InterruptControl) -> Self {
         v as u8
     }
 }
 
+/// Square wave output frequency options.
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum SquareWaveFrequency {
+    /// 1 Hz square wave output
     Hz1 = 0b00,
+    /// 1.024 kHz square wave output
     Hz1024 = 0b01,
+    /// 4.096 kHz square wave output
     Hz4096 = 0b10,
+    /// 8.192 kHz square wave output
     Hz8192 = 0b11,
 }
 impl From<u8> for SquareWaveFrequency {
+    /// Creates a SquareWaveFrequency from a raw register value.
+    ///
+    /// # Panics
+    /// Panics if the value is not 0b00, 0b01, 0b10, or 0b11.
     fn from(v: u8) -> Self {
         match v {
             0b00 => SquareWaveFrequency::Hz1,
@@ -229,6 +293,7 @@ impl From<u8> for SquareWaveFrequency {
     }
 }
 impl From<SquareWaveFrequency> for u8 {
+    /// Converts a SquareWaveFrequency to its raw register value.
     fn from(v: SquareWaveFrequency) -> Self {
         v as u8
     }
@@ -252,80 +317,111 @@ macro_rules! from_register_u8 {
 }
 
 bitfield! {
+    /// Seconds register (0-59) with BCD encoding.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Seconds(u8);
     impl Debug;
+    /// Tens place of seconds (0-5)
     pub ten_seconds, set_ten_seconds: 6, 4;
+    /// Ones place of seconds (0-9)
     pub seconds, set_seconds: 3, 0;
 }
 from_register_u8!(Seconds);
 
 bitfield! {
+    /// Minutes register (0-59) with BCD encoding.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Minutes(u8);
     impl Debug;
+    /// Tens place of minutes (0-5)
     pub ten_minutes, set_ten_minutes: 6, 4;
+    /// Ones place of minutes (0-9)
     pub minutes, set_minutes: 3, 0;
 }
 from_register_u8!(Minutes);
 
 bitfield! {
+    /// Hours register with format selection and BCD encoding.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Hours(u8);
     impl Debug;
+    /// Time representation format (12/24 hour)
     pub from into TimeRepresentation, time_representation, set_time_representation: 6, 6;
+    /// PM flag (12-hour) or 20-hour bit (24-hour)
     pub pm_or_twenty_hours, set_pm_or_twenty_hours: 5, 5;
+    /// Tens place of hours
     pub ten_hours, set_ten_hours: 4, 4;
+    /// Ones place of hours
     pub hours, set_hours: 3, 0;
 }
 from_register_u8!(Hours);
 
 bitfield! {
+    /// Day of week register (1-7).
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Day(u8);
     impl Debug;
+    /// Day of week (1-7)
     pub day, set_day: 2, 0;
 }
 from_register_u8!(Day);
 
 bitfield! {
+    /// Date register (1-31) with BCD encoding.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Date(u8);
     impl Debug;
+    /// Tens place of date (0-3)
     pub ten_date, set_ten_date: 5, 4;
+    /// Ones place of date (0-9)
     pub date, set_date: 3, 0;
 }
 from_register_u8!(Date);
 
 bitfield! {
+    /// Month register (1-12) with century flag and BCD encoding.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Month(u8);
     impl Debug;
+    /// Century flag (1 = year 2000+)
     pub century, set_century: 7;
+    /// Tens place of month (0-1)
     pub ten_month, set_ten_month: 4, 4;
+    /// Ones place of month (0-9)
     pub month, set_month: 3, 0;
 }
 from_register_u8!(Month);
 
 bitfield! {
+    /// Year register (0-99) with BCD encoding.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Year(u8);
     impl Debug;
+    /// Tens place of year (0-9)
     pub ten_year, set_ten_year: 7, 4;
+    /// Ones place of year (0-9)
     pub year, set_year: 3, 0;
 }
 from_register_u8!(Year);
 
 bitfield! {
+    /// Control register for device configuration.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Control(u8);
     impl Debug;
+    /// Oscillator enable/disable control
     pub from into Ocillator, oscillator_enable, set_oscillator_enable: 7, 7;
+    /// Enable square wave output on battery power
     pub battery_backed_square_wave, set_battery_backed_square_wave: 6;
+    /// Force temperature conversion
     pub convert_temperature, set_convert_temperature: 5;
+    /// Square wave output frequency selection
     pub from into SquareWaveFrequency, square_wave_frequency, set_square_wave_frequency: 4, 3;
+    /// INT/SQW pin function control
     pub from into InterruptControl, interrupt_control, set_interrupt_control: 2, 2;
+    /// Enable alarm 2 interrupt
     pub alarm2_interrupt_enable, set_alarm2_interrupt_enable: 1;
+    /// Enable alarm 1 interrupt
     pub alarm1_interrupt_enable, set_alarm1_interrupt_enable: 0;
 }
 from_register_u8!(Control);
@@ -363,37 +459,49 @@ impl defmt::Format for Control {
 }
 
 bitfield! {
+    /// Status register for device state and flags.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Status(u8);
     impl Debug;
+    /// Oscillator stop flag
     pub oscillator_stop_flag, set_oscillator_stop_flag: 7;
+    /// Enable 32kHz output
     pub enable_32khz_output, set_enable_32khz_output: 3;
+    /// Device busy flag
     pub busy, set_busy: 2;
+    /// Alarm 2 triggered flag
     pub alarm2_flag, set_alarm2_flag: 1;
+    /// Alarm 1 triggered flag
     pub alarm1_flag, set_alarm1_flag: 0;
 }
 from_register_u8!(Status);
 
 bitfield! {
+    /// Aging offset register for oscillator adjustment.
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct AgingOffset(u8);
     impl Debug;
+    /// Aging offset value (-128 to +127)
     pub i8, aging_offset, set_aging_offset: 7, 0;
 }
 from_register_u8!(AgingOffset);
 
 bitfield! {
+    /// Temperature register (integer part).
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct Temperature(u8);
     impl Debug;
+    /// Temperature value (-128 to +127)
     pub i8, temperature, set_temperature: 7, 0;
 }
 from_register_u8!(Temperature);
 
 bitfield! {
+    /// Temperature fraction register (decimal part).
     #[derive(Clone, Copy, Default, PartialEq)]
     pub struct TemperatureFraction(u8);
     impl Debug;
+    /// Temperature fraction value (0.00 to 0.99)
     pub temperature_fraction, set_temperature_fraction: 7, 0;
 }
 from_register_u8!(TemperatureFraction);
@@ -403,6 +511,12 @@ macro_rules! set_and_get_register {
         $(
             paste::item!{
                 #[cfg(not(feature = "async"))]
+                #[doc = concat!("Sets the value of the ", stringify!($name), " register.")]
+                #[doc = "\n\n# Arguments"]
+                #[doc = concat!("* `value` - The value to write to the ", stringify!($name), " register")]
+                #[doc = "\n\n# Returns"]
+                #[doc = "* `Ok(())` on success"]
+                #[doc = "* `Err(DS3231Error)` on error"]
                 pub fn [< set_ $name >](&mut self, value: $typ) -> Result<(), DS3231Error<I2C::Error>> {
                     self.i2c.write(
                         self.address,
@@ -410,7 +524,14 @@ macro_rules! set_and_get_register {
                         )?;
                     Ok(())
                 }
+
                 #[cfg(feature = "async")]
+                #[doc = concat!("Sets the value of the ", stringify!($name), " register (async version).")]
+                #[doc = "\n\n# Arguments"]
+                #[doc = concat!("* `value` - The value to write to the ", stringify!($name), " register")]
+                #[doc = "\n\n# Returns"]
+                #[doc = "* `Ok(())` on success"]
+                #[doc = "* `Err(DS3231Error)` on error"]
                 pub async fn [< set_ $name >](&mut self, value: $typ) -> Result<(), DS3231Error<I2C::Error>> {
                     self.i2c.write(
                         self.address,
@@ -422,13 +543,22 @@ macro_rules! set_and_get_register {
             }
 
             #[cfg(not(feature = "async"))]
+            #[doc = concat!("Gets the value of the ", stringify!($name), " register.")]
+            #[doc = "\n\n# Returns"]
+            #[doc = concat!("* `Ok(", stringify!($typ), ")` - The register value on success")]
+            #[doc = "* `Err(DS3231Error)` on error"]
             pub fn $name(&mut self) -> Result<$typ, DS3231Error<I2C::Error>> {
                 let mut data = [0];
                 self.i2c
                     .write_read(self.address, &[$regaddr as u8], &mut data)?;
                 Ok(paste::item!([<$typ>])(data[0]))
             }
+
             #[cfg(feature = "async")]
+            #[doc = concat!("Gets the value of the ", stringify!($name), " register (async version).")]
+            #[doc = "\n\n# Returns"]
+            #[doc = concat!("* `Ok(", stringify!($typ), ")` - The register value on success")]
+            #[doc = "* `Err(DS3231Error)` on error"]
             pub async fn $name(&mut self) -> Result<$typ, DS3231Error<I2C::Error>> {
                 let mut data = [0];
                 self.i2c
@@ -440,18 +570,26 @@ macro_rules! set_and_get_register {
     }
 }
 
+/// Error type for DS3231 operations.
 #[derive(Debug)]
 pub enum DS3231Error<I2CE> {
+    /// I2C bus error
     I2c(I2CE),
+    /// DateTime validation or conversion error
     DateTime(DS3231DateTimeError),
 }
 
 impl<I2CE> From<I2CE> for DS3231Error<I2CE> {
+    /// Creates a DS3231Error from an I2C error.
     fn from(e: I2CE) -> Self {
         DS3231Error::I2c(e)
     }
 }
 
+/// DS3231 Real-Time Clock driver.
+///
+/// This struct provides the main interface to the DS3231 RTC device.
+/// It supports both blocking and async I2C operations through feature flags.
 pub struct DS3231<I2C: I2c> {
     i2c: I2C,
     address: u8,
@@ -460,6 +598,11 @@ pub struct DS3231<I2C: I2c> {
 
 #[allow(unused)]
 impl<I2C: I2c> DS3231<I2C> {
+    /// Creates a new DS3231 driver instance.
+    ///
+    /// # Arguments
+    /// * `i2c` - The I2C bus implementation
+    /// * `address` - The I2C address of the device (typically 0x68)
     pub fn new(i2c: I2C, address: u8) -> Self {
         Self {
             i2c,
@@ -468,6 +611,14 @@ impl<I2C: I2c> DS3231<I2C> {
         }
     }
 
+    /// Configures the device according to the provided configuration.
+    ///
+    /// # Arguments
+    /// * `config` - The configuration to apply
+    ///
+    /// # Returns
+    /// * `Ok(())` on success
+    /// * `Err(DS3231Error)` on error
     #[cfg(not(feature = "async"))]
     pub fn configure(&mut self, config: &Config) -> Result<(), DS3231Error<I2C::Error>> {
         let mut control = self.control()?;
@@ -486,6 +637,14 @@ impl<I2C: I2c> DS3231<I2C> {
         Ok(())
     }
 
+    /// Configures the device according to the provided configuration (async version).
+    ///
+    /// # Arguments
+    /// * `config` - The configuration to apply
+    ///
+    /// # Returns
+    /// * `Ok(())` on success
+    /// * `Err(DS3231Error)` on error
     #[cfg(feature = "async")]
     pub async fn configure(&mut self, config: &Config) -> Result<(), DS3231Error<I2C::Error>> {
         #[cfg(any(feature = "log", feature = "defmt"))]
@@ -508,6 +667,11 @@ impl<I2C: I2c> DS3231<I2C> {
     }
 
     #[cfg(not(feature = "async"))]
+    /// Reads the raw datetime registers from the device.
+    ///
+    /// # Returns
+    /// * `Ok(DS3231DateTime)` - The raw datetime values on success
+    /// * `Err(DS3231Error)` on error
     fn read_raw_datetime(&mut self) -> Result<DS3231DateTime, DS3231Error<I2C::Error>> {
         let mut data = [0; 7];
         self.i2c
@@ -516,6 +680,11 @@ impl<I2C: I2c> DS3231<I2C> {
     }
 
     #[cfg(feature = "async")]
+    /// Reads the raw datetime registers from the device (async version).
+    ///
+    /// # Returns
+    /// * `Ok(DS3231DateTime)` - The raw datetime values on success
+    /// * `Err(DS3231Error)` on error
     async fn read_raw_datetime(&mut self) -> Result<DS3231DateTime, DS3231Error<I2C::Error>> {
         let mut data = [0; 7];
         self.i2c
@@ -525,6 +694,14 @@ impl<I2C: I2c> DS3231<I2C> {
     }
 
     #[cfg(not(feature = "async"))]
+    /// Writes raw datetime values to the device registers.
+    ///
+    /// # Arguments
+    /// * `datetime` - The raw datetime values to write
+    ///
+    /// # Returns
+    /// * `Ok(())` on success
+    /// * `Err(DS3231Error)` on error
     fn write_raw_datetime(
         &mut self,
         datetime: &DS3231DateTime,
@@ -547,6 +724,14 @@ impl<I2C: I2c> DS3231<I2C> {
     }
 
     #[cfg(feature = "async")]
+    /// Writes raw datetime values to the device registers (async version).
+    ///
+    /// # Arguments
+    /// * `datetime` - The raw datetime values to write
+    ///
+    /// # Returns
+    /// * `Ok(())` on success
+    /// * `Err(DS3231Error)` on error
     async fn write_raw_datetime(
         &mut self,
         datetime: &DS3231DateTime,
@@ -570,18 +755,36 @@ impl<I2C: I2c> DS3231<I2C> {
         Ok(())
     }
 
+    /// Gets the current date and time from the device.
+    ///
+    /// # Returns
+    /// * `Ok(DateTime<Utc>)` - The current date and time in UTC
+    /// * `Err(DS3231Error)` on error
     #[cfg(not(feature = "async"))]
     pub fn datetime(&mut self) -> Result<DateTime<Utc>, DS3231Error<I2C::Error>> {
         let raw = self.read_raw_datetime()?;
         raw.into_datetime().map_err(DS3231Error::DateTime)
     }
 
+    /// Gets the current date and time from the device (async version).
+    ///
+    /// # Returns
+    /// * `Ok(DateTime<Utc>)` - The current date and time in UTC
+    /// * `Err(DS3231Error)` on error
     #[cfg(feature = "async")]
     pub async fn datetime(&mut self) -> Result<DateTime<Utc>, DS3231Error<I2C::Error>> {
         let raw = self.read_raw_datetime().await?;
         raw.into_datetime().map_err(DS3231Error::DateTime)
     }
 
+    /// Sets the current date and time on the device.
+    ///
+    /// # Arguments
+    /// * `datetime` - The date and time to set
+    ///
+    /// # Returns
+    /// * `Ok(())` on success
+    /// * `Err(DS3231Error)` on error
     #[cfg(not(feature = "async"))]
     pub fn set_datetime(
         &mut self,
@@ -593,6 +796,14 @@ impl<I2C: I2c> DS3231<I2C> {
         Ok(())
     }
 
+    /// Sets the current date and time on the device (async version).
+    ///
+    /// # Arguments
+    /// * `datetime` - The date and time to set
+    ///
+    /// # Returns
+    /// * `Ok(())` on success
+    /// * `Err(DS3231Error)` on error
     #[cfg(feature = "async")]
     pub async fn set_datetime(
         &mut self,
